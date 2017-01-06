fb605181a119462ac9566f61503f82346434c281
Carl Crawford <GlowingElephantMedia@Gmail.com>
2017-01-06 14:43:59 +0000

    Click to move functionality enabled

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Controller player;
+
+	private Vector3 offset = new Vector3 (-5, 10, -5);
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Camera.main.transform.position = offset + player.transform.position;
+		
+	}
+}
---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+	public GameObject goTerrain;
+
+	Vector3 TargetPos ()
+	{
+		Vector3 tPos = Vector3.zero;
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (goTerrain.GetComponent<Collider> ().Raycast (ray, out hit, Mathf.Infinity)) {
+			tPos = hit.point;
+		}
+		return tPos;
+	}
+
+	NavMeshAgent agent;
+
+	void Update ()
+	{
+		if (agent == null) {
+			agent = GetComponent<NavMeshAgent> ();
+		}
+
+		if (Input.GetMouseButton (0)) {
+			Move (TargetPos ());
+		}
+	}
+
+	void Move (Vector3 target)
+	{
+		agent.SetDestination (target);
+	}
+}
